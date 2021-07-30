ac7df1e1e5e7529dc8af5161f69b650a1e28800c
Dez <dezlin77@users.noreply.github.com>
2021-07-30 09:48:56 +0800

    Create randomWalk.py

---
+++ py2021/turtlePractise/randomWalk.py
@@ -0,0 +1,19 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+
+colors = [
+"CornflowerBlue",
+"DarkOrchid",
+"IndianRed", 
+"DeepSkyBlue",
+"LightSeaGreen",
+"wheat",
+"SlateGray",
+"SeaGreen"
+]
+
+
+
+
