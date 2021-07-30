e2007ce441c84f8251b35848b3348ae9b69390d4
Dez <dezlin77@users.noreply.github.com>
2021-07-30 17:31:00 +0800

    Create spirograph.py

---
+++ py2021/turtlePractise/spirograph.py
@@ -0,0 +1,21 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+
+def random_color():
+  r = random.randint(0, 255)
+  g = random.randint(0, 255)
+  b = random.randint(0, 255)
+  color = (r, g, b)
+  return color
+
+def draw_spiro(gap_size):
+  for _ in range(int( 360 / gap_size)):
+    tim.color(random_color())
+    tim.circle(100)
+    tim.setheading(tim.heading() + gap_size)
+    
+draw_spiro(5)
+  
