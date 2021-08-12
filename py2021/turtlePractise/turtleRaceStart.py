f230ce0bd28cd389877af53699cf992507d7065f
Dez <dezlin77@users.noreply.github.com>
2021-08-12 09:29:19 +0800

    Update turtleRaceStart.py

--- py2021/turtlePractise/turtleRaceStart.py
+++ py2021/turtlePractise/turtleRaceStart.py
@@ -1,1 +1,26 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win race? Enter color: ")
+colors = [
+"red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "purple"
+]
+y_positions = []
+
+for turtle_index in range(0,6):
+  tim = Turtle(shape = "turtle")
+  timp.penup()
+  tim.goto(x=-230, y=-100)
+  
+ 
+screen.exitonclick()
+
+
+
 
