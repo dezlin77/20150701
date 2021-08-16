3a3c1207de63db92bf9eb225c3ad0a992368e080
Dez <dezlin77@users.noreply.github.com>
2021-08-16 16:01:59 +0800

    Update stuff.py

--- randoms/stuff.py
+++ randoms/stuff.py
@@ -9,6 +9,10 @@ with open("text1.txt") as file:
   contents = file.read()
   print(contents)
 
+with open("text1.txt", mode="a") as file:
+  file.write("\nnew text.")
+  
+
 
 #https://brucefwebster.com/2013/09/13/the-real-software-crisis-byte-magazine-january-1996/
 
