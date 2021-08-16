f4db2a7925924df1db5c88a3980fc36414e0043f
Dez <dezlin77@users.noreply.github.com>
2021-08-16 16:53:48 +0800

    Update stuff.py

--- randoms/stuff.py
+++ randoms/stuff.py
@@ -18,7 +18,17 @@ with open("weather_data.csv") as data_file:
   data = data_file.readlines()
   print(data)
 
+#
+import csv
 
+with open("weather_data.csv") as data_file:
+  data = csv.reader(data_file)
+  for row in data:
+    print(row)
+
+  
+  
+  
 #https://brucefwebster.com/2013/09/13/the-real-software-crisis-byte-magazine-january-1996/
 
 #https://brucefwebster.com/2008/01/10/the-wetware-crisis-tepes/
