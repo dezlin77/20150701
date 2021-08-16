59a0f73631c2257b501ddd49d6b073635dee75a2
Dez <dezlin77@users.noreply.github.com>
2021-08-16 15:53:57 +0800

    Update and rename stuff to stuff.py

---
+++ randoms/stuff.py
@@ -0,0 +1,14 @@
+
+file = open("text1.txt")
+contents = file.read()
+print(contents)
+
+
+
+
+
+#https://brucefwebster.com/2013/09/13/the-real-software-crisis-byte-magazine-january-1996/
+
+#https://brucefwebster.com/2008/01/10/the-wetware-crisis-tepes/
+
+
