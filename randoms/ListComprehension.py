73d3efceeac54457ca54af93d89d14a3045045db
Dez <dezlin77@users.noreply.github.com>
2021-08-18 17:24:48 +0800

    Update ListComprehension.py

--- randoms/ListComprehension.py
+++ randoms/ListComprehension.py
@@ -6,3 +6,11 @@ squared_numbers = [n*n for n in numbers]
 
 print(squared_numbers)
 
+#filter even numbers
+
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+even_numbers = [n for n in numbers if n % 2 == 0]
+
+print(even_numbers)
+
