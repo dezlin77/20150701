099ec0a65db66121f8cfc683a0a1e070a67a0a3c
Dez <dezlin77@users.noreply.github.com>
2021-08-19 17:06:18 +0800

    Update DictComprehension.py

--- randoms/DictComprehension.py
+++ randoms/DictComprehension.py
@@ -1,3 +1,9 @@
 #new_dict ={new_key: new_value for item in list}
 
+names = ['Alex', 'Brad', 'Chelsea', 'Don', 'Edward', 'Fred']
+import random
+student_scores = {student:random.randint(1, 100) for student in names}
+
+
+
 
