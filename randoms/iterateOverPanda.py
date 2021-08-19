f91086cc42dc888b7e7c3de9a100392f29ae7ea5
Dez <dezlin77@users.noreply.github.com>
2021-08-19 17:20:03 +0800

    Update iterateOverPanda.py

--- randoms/iterateOverPanda.py
+++ randoms/iterateOverPanda.py
@@ -1,3 +1,27 @@
+student_dict = {
+    "studnet": ['Alfred', 'Bob', 'Cam'],
+    "score": [58, 62, 78],
+}
+
+import pandas
+
+student_data_frame = pandas.DataFrame(student_dict)
+#print(student_data_frame)
+
+for (index, row) in student_data_frame.iterrows():
+#    print(row)
+    if row.student == "Angela":
+        print(row.score)
+
+
+
+
+
+
+
+
+
+
 #For Loop
 numbers = [1, 2, 3]
 new_list = []
