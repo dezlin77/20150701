e7cd911f2418b93afa1835f5ccd90708c3494c30
Dez <dezlin77@users.noreply.github.com>
2021-08-19 17:12:00 +0800

    Update DictComprehension.py

--- randoms/DictComprehension.py
+++ randoms/DictComprehension.py
@@ -4,6 +4,16 @@ names = ['Alex', 'Brad', 'Chelsea', 'Don', 'Edward', 'Fred']
 import random
 student_scores = {student:random.randint(1, 100) for student in names}
 
+#check out which students passed the test, got over 60 points
+#passed_students = { int(student) > 60 for student in student_scores}
+passed_students = { student:score for (student, score) in student_scores.items() if score >=60}
+
+#take each word in given sentence and calculate the number of letters in each word
+sentence = "hi how are you how do you do?"
+result = {word:len(word) for word in sentence.split()}
+print(result)
+
+
 
 
 
