30feaf39c120d4bf0957ca8bf7a1705eeeb8c969
Dez <dezlin77@users.noreply.github.com>
2021-08-20 16:44:15 +0800

    Update kwargsAdd.py

--- randoms/kwargsAdd.py
+++ randoms/kwargsAdd.py
@@ -1,8 +1,30 @@
 #many keyworded arguments
 
-def calculate (**)
+# How to use a **kwargs dictionary safely
+class Car:
+
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.colour = kw.get("colour")
+        self.seats = kw.get("seats")
+
+my_car = Car(make="Nissan", model="Skyline")
+print(my_car.model)
 
 
 
+
+
+def calculate (n, **kwargs):
+  print(kwargs)
+#  for key, value in kwargs.items():
+#    print(key)
+#    print(value)
+  n += kwargs["add"]
+  n *= kwargs["multiply"]
+  print(n)
+
+calculate(add=3, multiply=5)
 
 
