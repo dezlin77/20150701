2a1c95f4ff93e0ef4eae382b762c48ebc8bc9cda
Dez <dezlin77@users.noreply.github.com>
2021-08-20 16:30:07 +0800

    Update CreateWindow.py

--- randoms/CreateWindow.py
+++ randoms/CreateWindow.py
@@ -1,6 +1,16 @@
 import tkinter
 
 window = tkinter.Tk()
+window.title("My first GUI")
+window.minsize(width=500, height=300)
+
+#label
+
+my_label = tkinter.Label(text="I'm am a label", font=("Arial", 24, "bold"))
+my_label.pack(expand=True)
+
+tim = turtle.Turtle()
+tim.write()
 
 window.mainloop()
 
