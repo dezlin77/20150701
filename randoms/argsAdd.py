2944c96dbc8307b5a4faee3e253f63734371a70d
Dez <dezlin77@users.noreply.github.com>
2021-08-20 16:34:31 +0800

    Update argsAdd.py

--- randoms/argsAdd.py
+++ randoms/argsAdd.py
@@ -1,10 +1,13 @@
+#*args is positional, tuple, refer to them by tuple position.
+#if you want to refer to them by name, use **kwargs
+
 def add(*args):
   sum = 0
   for n in args:
     sum += n
   return sum
 
-    
+#args is a tuple fyi
     
 print(add(3, 5, 7, 11, 17))
 
