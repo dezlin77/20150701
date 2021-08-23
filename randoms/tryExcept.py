2b38c0b817788ab9d0ae89272ea0391b4b5c1a0a
Dez <dezlin77@users.noreply.github.com>
2021-08-23 17:30:20 +0800

    Create tryExcept.py

---
+++ randoms/tryExcept.py
@@ -0,0 +1,7 @@
+
+#try means to try this line of code
+try:
+  file =open("a_file.txt")
+#except means if the try line of code didn't work, then do the line of code in the except
+except:
+  print("There's an error")
