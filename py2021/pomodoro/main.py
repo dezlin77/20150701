4c83c59725b6ac13db88c4d4c20049e369495ae6
Dez <dezlin77@users.noreply.github.com>
2021-08-23 16:29:04 +0800

    Create main.py

---
+++ py2021/pomodoro/main.py
@@ -0,0 +1,21 @@
+from tkinter import *
+import math
+#using tkinter and canvas widget is a JOKE! this is ridiculous!
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 1
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+
+
+
