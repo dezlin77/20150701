a5e324eef3623edc6358c26fcf8378ddcb400790
Dez <dezlin77@users.noreply.github.com>
2021-08-24 10:44:10 +0800

    Update 20210824_BMIraiseError.py

--- randoms/20210824_BMIraiseError.py
+++ randoms/20210824_BMIraiseError.py
@@ -10,3 +10,16 @@ if height > 3:
 bmi = weight. height ** 2
 print(bmi)
 
+
+#new index error
+fruits = ["Apple", "Pear", "Orange"]
+
+def make_pie(index):
+  try:
+    fruit = fruits[index]
+  except IndexError:
+    print("Fruit pie")
+  else:
+    print(fruit + "pie")
+    
+make_pie(4)
