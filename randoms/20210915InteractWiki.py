9e88ad5164c9d539572e26c1541757ee7ae285a2
Dez <dezlin77@users.noreply.github.com>
2021-09-16 16:29:07 +0800

    Update 20210915InteractWiki.py

--- randoms/20210915InteractWiki.py
+++ randoms/20210915InteractWiki.py
@@ -3,4 +3,5 @@ from selenium import webdriver
 chrome_driver_path = "users/kenny/dev/chromedriver"
 driver = webdriver.Chrome(chrome_driver_path)
 
-
+driver.get("https://en.wikipedia.org/wiki/Main_Page")
+article_count = driver.find_element_by_css_selector("#articlecount a")
