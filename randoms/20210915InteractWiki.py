6416bb5ca58dcfbfd8fb2fdd6aa81873fb1def2b
Dez <dezlin77@users.noreply.github.com>
2021-09-16 17:56:30 +0800

    Update 20210915InteractWiki.py

--- randoms/20210915InteractWiki.py
+++ randoms/20210915InteractWiki.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 chrome_driver_path = "users/kenny/dev/chromedriver"
 driver = webdriver.Chrome(chrome_driver_path)
@@ -6,5 +7,17 @@ driver = webdriver.Chrome(chrome_driver_path)
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 article_count = driver.find_element_by_css_selector("#articlecount a")
 #print(article_count.text)
-article_count.click()
+#article_count.click()
+
+all_portals = find_element_by_link_text("all portals")
+#all_portals.click()
+
+search = driver.find_element_by_name("search")
+search.send_keys("Python")
+search.send_keys(Keys.ENTER)
+
+
+=====================
+
+
 
