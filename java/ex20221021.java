74f9c53b94d2968e885b845de7690b8f76cb04c3
Dez <dezlin77@users.noreply.github.com>
2022-10-21 16:51:56 +0800

    Create ex20221021.java

---
+++ java/ex20221021.java
@@ -0,0 +1,11 @@
+/*
+    this is an example java 
+    program
+*/
+
+class Example {
+	// a java program begins with a call to main()
+	public static void main(String[] args) {
+		System.out.println("Java drives the Web.");
+	}
+}
