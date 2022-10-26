f2a39b9d84b00b91681c13e54bd4c43b59486492
Dez <dezlin77@users.noreply.github.com>
2022-10-26 15:43:00 +0800

    Update ex20221021.java

--- java/ex20221021.java
+++ java/ex20221021.java
@@ -9,3 +9,23 @@ class Example {
 		System.out.println("Java drives the Web.");
 	}
 }
+
+//loops
+while (x > 12) {
+    x = x + 1; //code will run when bigger than 12, then minus one or we'd be running forever
+}
+
+for (int i = 0; i<10; i=i+1){
+    System.out.print("i is now " + i);
+}
+
+//branching, if/else
+if (x==10) {
+  System.out.print("X must be 10");
+} else {
+  System.out.print("X isn't 10);
+}
+System.out.print("this line runs no matter what");
+
+
+
